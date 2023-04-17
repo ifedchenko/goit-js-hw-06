10135a122fe11c36c2681aacaf3fa768671d4dcf
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-04-17 20:57:22 +0300

    Finish first hw

---
+++ js/task-01.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+const categoriesList = document.querySelector('#categories');
+const items = categoriesList.querySelectorAll('.item')
+
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach(item => {
+
+    const categoryName = item.querySelector('h2').textContent;
+    const elementsAmount = item.querySelectorAll('li').length;
+
+    console.log(`Category: ${categoryName}`)
+    console.log(`Elements: ${elementsAmount}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
