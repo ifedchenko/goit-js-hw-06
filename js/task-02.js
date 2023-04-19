d80c6ac6c0fa972cdea5db3a919b0b6d8d05ffe4
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-04-19 18:07:30 +0300

    Bugfix task-2

--- js/task-02.js
+++ js/task-02.js
@@ -14,7 +14,7 @@ const ingredientsList = document.querySelector('#ingredients');
 const elements = ingredients.map((ingredient) =>{
   const element = document.createElement('li');
   element.textContent = ingredient
-  element.classList.add('ingredient');
+  element.classList.add('item');
   return element;
 })
 
