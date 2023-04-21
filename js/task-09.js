558d7a18d3ce954253c1610a492a9adfba1f4624
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-04-22 01:25:25 +0300

    Finish task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+'use strict';
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+
+const btnChangeColor = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color')
+
+btnChangeColor.addEventListener('click', onButtonClick)
+function onButtonClick() {
+  document.body.style.backgroundColor = getRandomHexColor()
+  colorValue.textContent= getRandomHexColor()
 }
