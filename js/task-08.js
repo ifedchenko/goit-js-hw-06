2d7cf51474df85edafef61caea01493d47ca4e61
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-04-21 23:00:58 +0300

    Finish task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+'use strict';
 
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements
+    const email = formElements.email.value
+    const password = formElements.password.value
+    const formData = {
+        email, password,
+    }
+
+        if (email.length === 0 || password.length === 0){
+            alert('Please enter all fields')
+        } else {
+            console.log(formData);
+            form.reset();
+        }
+}
