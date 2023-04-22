a347579348a1c8456a2fe7c0dfff32e30a1a4bc7
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-04-22 20:57:55 +0300

    Finish task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const createBtn = document.querySelector('[data-create]')
+const destroyBtn = document.querySelector('[data-destroy]')
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const boxDiv = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  let baseSize = 30;
+  for (let i = 0; i < amount; i+=1) {
+    const div = document.createElement('div');
+    div.style.width = `${baseSize}px`;
+    div.style.height = `${baseSize}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxDiv.append(div);
+    baseSize += 10;
+  }
+}
+createBtn.addEventListener('click', () => {
+    if (input.value === '') {
+     alert('Please type number')
+    } else {
+      createBoxes(input.value);
+      input.value = '';
+    }
+})
+
+destroyBtn.addEventListener('click', () => {
+  boxDiv.innerHTML = '';
+})
