8d57c74c3340c78bc4a4b910097b5f30bd59cf1b
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-04-23 14:18:56 +0300

    Bugfix task 1

--- js/task-01.js
+++ js/task-01.js
@@ -8,8 +8,11 @@ console.log(`Number of categories: ${items.length}`);
 
 items.forEach(item => {
 
-    const categoryName = item.querySelector('h2').textContent;
-    const elementsAmount = item.querySelectorAll('li').length;
+    const categoryName = item.children.item(0).textContent;
+    const elementsAmount = item.lastElementChild.children.length
+
+    // const categoryName = item.querySelector('h2').textContent;
+    // const elementsAmount = item.querySelectorAll('li').length;
 
     console.log(`Category: ${categoryName}`)
     console.log(`Elements: ${elementsAmount}`)
